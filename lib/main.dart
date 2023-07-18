19927680e33831dcca333d31d719ccf24b76d9b3
andysooho <andysooho@gmail.com>
2023-07-19 02:20:49 +0900

    feat: Riverpod v1 complete
    riverpod v1 practice complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpodv2_gorouter/riverpod/provider/provider_observer.dart';
 import 'package:riverpodv2_gorouter/riverpod/screen/home_screen.dart';
 
 void main() {
   runApp(
-      const ProviderScope(
-        child: MaterialApp(
+      ProviderScope(
+        observers: [
+          Logger(),
+        ],
+        child: const MaterialApp(
           home: HomeScreen(),
         ),
       )
